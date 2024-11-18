775ec93e935a82cd824a3883d8729f3af602ce40
Bruno Gomes Laurentino <bruno.laurentino.adm@gmail.com>
2024-11-18 16:01:57 -0300

    Programação Orientada a Objetos

---
+++ Poo/src/exemplo1/Exemplo2/MSNMensseger.java
@@ -0,0 +1,16 @@
+package exemplo1.Exemplo2;
+
+public class MSNMensseger {
+    public void enviarMensagem(){
+        System.out.println("ENVIANDO MENSAGEM!");
+    }
+    public void receberMensagem(){
+        System.out.println("RECEBENDO MENSAGEM!");
+    }
+    public void validarConectadoInternet(){
+        System.out.println("VALIDANDO CONEXÃO À INTERNET");
+    }
+    public void salvarHistoricoMensagem(){
+        System.out.println("SALVANDO HISTÓRICO DE MENSAGENS!");
+    }
+}
